f072a9d5b93cf14db9fa5e3fcb193c1b0c2ddc68
Jay Evans <james@cloudskyinc.com>
2022-02-26 10:52:24 +0000

    feat: Adds styling and placeholder with default value

--- src/modules/shared/inputComponent/InputComponent.vue
+++ src/modules/shared/inputComponent/InputComponent.vue
@@ -9,6 +9,7 @@
       :class="{ 'input--error': hasError }"
       :type="inputType"
       :name="name"
+      :placeholder="placeholder"
       @input="emitValue"
     >
     <div v-show="hasError" class="input__error-box" :data-test-id="`${testId}Error`">
@@ -32,9 +33,12 @@ interface Props {
   inputType: string;
   errorMessages: ErrorObject[];
   modelValue: string;
+  placeholder?: string;
 }
 
-defineProps<Props>()
+withDefaults(defineProps<Props>(), {
+  placeholder: ''
+})
 
 const emit = defineEmits<{
   (e: 'update:modelValue', value: string): void
@@ -47,3 +51,68 @@ function emitValue(event: Event) {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+label {
+  display: block;
+  margin-bottom: 15px;
+  text-transform: capitalize;
+  text-align: left;
+}
+
+input {
+  appearance: none;
+  border-radius: 6px;
+  padding: 10px;
+  background: transparent !important;
+  border: 1px solid var(--color-border-primary);
+  outline: 0;
+  color: var(--color-text-primary);
+  width: 100%;
+  transition: 250ms ease-in-out;
+
+  &::placeholder {
+    color: var(--color-text-placeholder);
+  }
+
+  &:focus {
+    outline: 0;
+    border-color: var(--color-border-active);
+    caret-color: var(--color-secondary);
+    background: var(--color-background-tertiary);
+  }
+
+  &:hover {
+    border-color: var(--color-border-active);
+  }
+}
+
+.input-group {
+  margin-bottom: 15px;
+}
+
+.label {
+  &--error {
+    color: var(--color-text-error);
+  }
+}
+
+.input {
+  &--error {
+    border-color: var(--color-border-error);
+  }
+
+  &__error-box {
+    background: var(--color-background-error);
+    border-radius: 6px;
+    padding: 15px;
+    margin-top: 15px;
+  }
+
+  &__error-box-text {
+    color: var(--color-text-error);
+  }
+}
+
+</style>
